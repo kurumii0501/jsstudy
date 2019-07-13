dea3d3d73e8e9cf1643d9a93c8e10692c7775bff
kurumii0501 <kurumii0501@icloud.com>
2019-07-14 01:35:01 +0900

    更新

---
+++ js/scope.js
@@ -0,0 +1,26 @@
+var x = 0;
+
+function func(){
+  //console.log( "関数内で参照(宣言前):" + x );
+  let x = 1;
+  console.log( "関数内で参照(宣言前)" + x );
+}
+
+func();
+console.log( "関数外で参照:" + x );
+
+let obj = {
+  name : "エーミール・シンクレール",
+  age :  10,
+};
+
+console.log( obj.name );
+
+let array = [1, 2, 3];
+console.log( array[1] );
+
+a=1,b=1,c=1
+
+for( var i = 0, j = 5; i  <= 5; i++, j-- ){
+  console.log(" i: " + " j: " + j );
+}
